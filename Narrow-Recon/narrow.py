6aaf87009f0685a6f573f2aba0f9be95584e71e8
ali razmkhah <33378699+rex1c@users.noreply.github.com>
2024-02-23 01:26:03 +0330

    Update narrow.py

--- Narrow-Recon/narrow.py
+++ Narrow-Recon/narrow.py
@@ -19,9 +19,9 @@ for domain in domains :
     os.system("echo '127.0.0.1 {}' >> /etc/hosts".format(localhost))
     os.system("echo '{}' | gospider --other-source --include-subs --subs --quiet | sort -u |  grep -Eiv '\.(css|js|jpg|jpeg|png|svg|img|gif|exe|apk|mp4|flv|pdf|doc|ogv|webm|wmv|webp|mov|mp3|m4a|m4p|ppt|pptx|scss|tif|tiff|ttf|otf|woff|woff2|bmp|ico|eot|htc|swf|rtf|image|rf)' >> gospider.txt".format(s))
     os.system("sed -i  '/127.0.0.1/d' /etc/hosts")
-    os.system("cat gospider.txt gau.txt | sort -u >> passive.narrow")
+    os.system("cat gospider.txt gau.txt | sort -u >> passive.narrow.txt")
     # Filter passive links by status code
-    os.system("httpx -silent -l passive.narrow -mc 200,301,302,303,304,403 > passive.narrow")
+    os.system("httpx -silent -l passive.narrow.txt -mc 200,301,302,303,304,403 -o passive.narrow")
     
 
     
